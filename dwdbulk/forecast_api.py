b89658de65b5335ea372053d842af98a065022f9
Jeremiah Lewis <4462211+jlewis91@users.noreploy.github.com>
2019-08-19 17:33:46 +0200

    Remove print statement.

--- dwdbulk/forecast_api.py
+++ dwdbulk/forecast_api.py
@@ -7,24 +7,24 @@ import numpy as np
 from lxml import etree
 import tempfile
 
+
 def fetch_weather_forecast(url, xml_directory_path="xml_zip"):
 
     if not os.path.exists(xml_directory_path):
         os.makedirs(xml_directory_path)
-
 
     r = requests.get(url, stream=True)
     file_name = "__".join(url.split(".de/", 1)[1].split("/"))
 
     if r.status_code == 200:
         with tempfile.TemporaryDirectory() as tmpdirname:
-            with open(tmpdirname + "/" + file_name, 'wb') as f:
+            with open(tmpdirname + "/" + file_name, "wb") as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
 
-            with ZipFile(tmpdirname + "/" + file_name, 'r') as zipObj:
-               # Extract all the contents of zip file in current directory
-               zipObj.extractall(path=xml_directory_path)
+            with ZipFile(tmpdirname + "/" + file_name, "r") as zipObj:
+                # Extract all the contents of zip file in current directory
+                zipObj.extractall(path=xml_directory_path)
 
 
 def convert_xml_to_parquet(path):
@@ -33,37 +33,58 @@ def convert_xml_to_parquet(path):
     tree = etree.parse(path)
     root = tree.getroot()
 
-
-    prod_items = {"product_id": "ProductID", "generating_process": "GeneratingProcess", "date_issued": "IssueTime"}
+    prod_items = {
+        "product_id": "ProductID",
+        "generating_process": "GeneratingProcess",
+        "date_issued": "IssueTime",
+    }
 
     # Get Basic Metadata
-    prod_definition = root.findall('kml:Document/kml:ExtendedData/dwd:ProductDefinition', root.nsmap)[0]
-    prod_items = {"product_id": "ProductID", "generating_process": "GeneratingProcess", "date_issued": "IssueTime"}
-    metadata = {k: prod_definition.find(f"{{{root.nsmap['dwd']}}}{v}").text for k,v in prod_items.items()}
+    prod_definition = root.findall(
+        "kml:Document/kml:ExtendedData/dwd:ProductDefinition", root.nsmap
+    )[0]
+    prod_items = {
+        "product_id": "ProductID",
+        "generating_process": "GeneratingProcess",
+        "date_issued": "IssueTime",
+    }
+    metadata = {
+        k: prod_definition.find(f"{{{root.nsmap['dwd']}}}{v}").text
+        for k, v in prod_items.items()
+    }
 
     # Get Time Steps
-    timesteps = root.findall('kml:Document/kml:ExtendedData/dwd:ProductDefinition/dwd:ForecastTimeSteps', root.nsmap)[0]
+    timesteps = root.findall(
+        "kml:Document/kml:ExtendedData/dwd:ProductDefinition/dwd:ForecastTimeSteps",
+        root.nsmap,
+    )[0]
     timesteps = [i.text for i in timesteps.getchildren()]
 
     # Get Station Forecasts
-    forecast_items = root.findall('kml:Document/kml:Placemark', root.nsmap)
+    forecast_items = root.findall("kml:Document/kml:Placemark", root.nsmap)
 
     for station_forecast in forecast_items:
         station_id = station_forecast.find("kml:name", root.nsmap).text
-        print(station_id)
-        measurement_list = station_forecast.findall("kml:ExtendedData/dwd:Forecast", root.nsmap)
+
+        measurement_list = station_forecast.findall(
+            "kml:ExtendedData/dwd:Forecast", root.nsmap
+        )
         df = pd.DataFrame({"date_start": timesteps})
 
         for measurement_item in measurement_list:
 
-            measurement_parameter = measurement_item.get(f"{{{root.nsmap['dwd']}}}elementName")
+            measurement_parameter = measurement_item.get(
+                f"{{{root.nsmap['dwd']}}}elementName"
+            )
             measurement_string = measurement_item.getchildren()[0].text
             measurement_values = " ".join(measurement_string.split()).split(" ")
 
-            assert len(measurement_values) == len(timesteps), "Number of timesteps does not match number of measurement values."
+            assert len(measurement_values) == len(
+                timesteps
+            ), "Number of timesteps does not match number of measurement values."
             df[measurement_parameter] = measurement_values
 
         df["station_id"] = station_id
-        for k,v in metadata.items():
+        for k, v in metadata.items():
             df[k] = v
         df.replace("-", np.nan, inplace=True)
